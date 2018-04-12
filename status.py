5921386e77315716869c04c1bbda3ec19c4c410f
Emily Ann Ediger <dje4321@gmail.com>
2018-04-12 15:27:19 -0500

    Added blacklist support for systemd and disk usage

--- status.py
+++ status.py
@@ -2,6 +2,11 @@
 import subprocess, os, sys
 argv = sys.argv
 
+def findArgv(argv,condidtion):
+    for x in range(0,len(argv)):
+        if argv[x] == condidtion:
+            return x
+
 def checkArgv(argv,condidition):
     for x in range(0,len(argv)):
         if argv[x] == condidition:
@@ -19,9 +24,13 @@ def applyBlacklist(command, blacklist):
     return command
 
 def checkSystemd(argv):
-    formattedOutput = 'The following issues were found with systemd\n\n'
+    formattedOutput = 'The following issues were found with systemd\n'
     systemctl,errors = [],0
     blacklist = []
+    
+    if checkArgv(argv,"--sysBlacklist") == True:
+        for x in range(0,len(argv[findArgv(argv,"--sysBlacklist") + 1].split(","))):
+            blacklist.append(argv[findArgv(argv,"--sysBlacklist") + 1].split(",")[x])
     
     systemctlOutput = subprocess.getoutput(applyBlacklist("systemctl",blacklist)).split('\n')
 
@@ -39,10 +48,18 @@ def checkSystemd(argv):
             os.system("zenity --error --ellipsize --text='{}' 2>/dev/null".format(formattedOutput))
 
 def diskUsage(argv):
-    formattedOutput = 'Low diskspace detected\n\n'
+    formattedOutput = 'Low diskspace detected\n'
     diskSpace,errors = [],0
-    threshold = 85 #Percent threshold for disk space usage
-    blacklist = ["snap"]
+    blacklist = []
+ 
+    if checkArgv(argv,"--diskBlacklist") == True:
+        for x in range(0,len(argv[findArgv(argv,"--diskBlacklist") + 1].split(","))):
+            blacklist.append(argv[findArgv(argv,"--diskBlacklist") + 1].split(",")[x])
+    
+    if checkArgv(argv,"--threshold") == True:
+        threshold = int(argv[findArgv(argv,"--threshold") + 1])
+    else:
+        threshold = 85 #Percent threshold for disk space usage
     
     dfOutput = subprocess.getoutput(applyBlacklist("df -h",blacklist)).split('\n')
 
@@ -63,11 +80,17 @@ def diskUsage(argv):
         if checkArgv(argv,"--nogui") == False:
             os.system("zenity --error --ellipsize --text='{}' 2>/dev/null".format(formattedOutput))
 
-if checkArgv(argv,"-h"):
+########################################################################################
+# Main Program
+
+if checkArgv(argv,"-h") == True or checkArgv(argv,"--help") == True:
     print(
 """{}
-    -h --help      Prints the help message
-    --nogui        Disables the GUI output and only prints to STDOUT""".format(argv[0]))
+    -h --help               Prints the help message
+    --nogui                 Disables the GUI output and only prints to STDOUT
+    --threshold             Overrides the threshold value for disk space usage
+    --diskBlacklist         Blacklists certain strings from disk usage checks. Values are comma separated
+    --sysBlacklist          Blacklists certain strings from systemd checks. Values are comma separated""".format(argv[0]))
     sys.exit()
 
 checkSystemd(argv)
