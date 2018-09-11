f5b37a6ca248c9a6808f53f143a2a5b549f71ec5
dje4321 <dje4321@gmail.com>
2018-09-10 21:42:30 -0500

    Added support for systemd blacklist

--- function.py
+++ function.py
@@ -8,12 +8,21 @@ def checkSystemd(config):
 
     print("[SYSTEMD]")
     systemctl = []
-    output = subprocess.getoutput("systemctl list-units").splitlines()
+    blacklist = config["systemdBlacklist"]
+    output = subprocess.getoutput("systemctl list-units").splitlines() #Get a list of all systemd units
 
-    for x in output:
+    for x in output: # Run over the systemd units and check if it has failed
         if x.count("failed") >= 1:
             # print(repr(x))
             systemctl.append(x.split(" ")[1] + " has failed")
+
+    if config["systemdBlacklist"] != []: # Apply the blacklist
+        counter = -1
+        counter += 1
+        for i in blacklist:
+            for a in systemctl:
+                if a.count(i) != 0:
+                    systemctl.pop(counter)
 
     for i in systemctl:
         print(i)
